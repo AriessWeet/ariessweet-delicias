b5fe579e10abbbd73d34da24132b12b0a8a0a8ce
AriessWeet <ariessweet32@gmail.com>
2025-04-09 08:04:20 -0300

    Corrijo scripts.js para que redireccione a home.html

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Función de bienvenida desde index.html
   const formulario = document.getElementById("formulario");
+
   if (formulario) {
     formulario.addEventListener("submit", function (e) {
       e.preventDefault();
@@ -13,22 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
       } else {
         alert("Por favor, escribí tu nombre");
       }
-    });
-  }
-
-  // Mostrar saludo en home.html
-  const saludo = document.getElementById("saludo-usuario");
-  const nombre = localStorage.getItem("usuario");
-  if (saludo && nombre) {
-    saludo.textContent = `¡Hola, ${nombre}! Bienvenid@ nuevamente 💖`;
-  }
-
-  // Botón cerrar sesión
-  const botonCerrarSesion = document.getElementById("cerrar-sesion");
-  if (botonCerrarSesion) {
-    botonCerrarSesion.addEventListener("click", () => {
-      localStorage.removeItem("usuario");
-      window.location.href = "index.html";
     });
   }
 });
